dc900be390518380b55f5b062308aab0c443ab88
Robert Oschwald <roos@symentis.com>
2013-07-01 12:48:03 +0200

    - comments

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -10,7 +10,7 @@ eventCreateWarStart = { warName, stagingDir ->
     println "Git Revision: ${gitRevision}"
   }
 
-  // branchCheck
+  // check if current branch is a valid one for this env. war creation.
   if (!config.scm.skipGitBranchCheck) {
     def currentBranchName =  GitClient.currentBranchName()
     assert currentBranchName in config.scm.validBranch, "\n\nCurrent branch >${GitClient.gitBranchName()}< is invalid for package, please checkout one of >${config.scm.validBranch}< \n\n"
@@ -19,21 +19,19 @@ eventCreateWarStart = { warName, stagingDir ->
     println "Skipping gitBranchCheck as scm.skipGitBranchCheck is set in Config.groovy"
   }
 
-  // commitCheck
+  // check that all files are committed
   if (!config.scm.skipGitCommitCheck){
     def uncommittedChanges = GitClient.uncommittedChanges()
     assert !uncommittedChanges, "\n\nYou have uncommitted changes!\n$uncommittedChanges"
   }
 
-  // PushedChanges check
-
-
-  // origin un-pulled changes
-  if (!config.scm.skipGitOriginCheck){
+  // check unpushed changes
+  if (!config.scm.skipGitPushedCheck){
      assert !GitClient.branchIsAheadOfRemote(), "Branch is ahead of origin!"
   }
 
-  if (!config.scm.skipGitPushedCheck) {
+  // check origin un-pulled changes
+  if (!config.scm.skipGitOriginCheck) {
     assert !GitClient.remoteIsAheadOfBranch(GitClient.currentBranchName()), "Origin is ahead of branch. Please perform pull"
   }
 
